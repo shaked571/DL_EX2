0253a80e5b111b47d8511483e67a6435c5d6f4b9
danity <danity251@gmail.com>
2021-05-05 13:17:06 +0300

    update main and arguments

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import argparse
-from tagger import MLP
-# Differnt schemes of training - { MLP + size of matrix
-# batch size (hyper paraeter - is there nore?)
-# lr
-# optimizer
-# hidden dim
+from tagger import MLP, Vocab, DataFile, Tranier
+from preprocessing import TitleProcess
 
 
-def main(train_path, test_path, part, batch_size, hidden_dim):
-    pass
+def main(train_path, dev_path, test_path, part, embedding_dim, batch_size, l_r, hidden_dim):
+    if part == 2:
+        embedding_dim = 50
+    title_process = TitleProcess()
+    vocab = Vocab(train_path)
+    train_df = DataFile(train_path, title_process, vocab)
+    dev_df = DataFile(dev_path, title_process, vocab)
+    test_df = DataFile(test_path, title_process, vocab)
+    model = MLP(embedding_dim, hidden_dim, vocab)
+    trainer = Tranier(model, train_df, dev_df, vocab, 15, batch_size, l_r)
+    trainer.train()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=False)
 
     parser.add_argument('--train_path', type=str, required=True)
+    parser.add_argument('--dev_path', type=str, required=True)
     parser.add_argument('--test_path', type=str, required=True)
     parser.add_argument('--part', type=int, required=True)
-    parser.add_argument('--batch_size', type=int, required=True)
+    parser.add_argument('--embedding_dim', type=int, required=False)
+    parser.add_argument('--batch_size', type=int, required=False)
+    parser.add_argument('--l_r', type=int, required=False)
     parser.add_argument('--hidden_dim', type=int, required=True)
 
     args = parser.parse_args()
 
-    main(args.train_path, args.test_path, args.part, args.batch_size, args.hidden_dim)
+    main(args.train_path, args.dev_path, args.test_path, args.part, args.embedding_dim, args.batch_size, args.l_r, args.hidden_dim)
