dc5ecbbb2098a9598245a6ce86860d1540d4fe14
shaked571 <shakedgreenfeld@gmail.com>
2021-05-05 21:11:55 +0300

    update run

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 which python
 
-for part in 1 3 4
+for part in 1 3
 do
  for task in ner pos
  do
-   for embedding_dim in 50 100 200
+   for embedding_dim in 100 200
    do
-    for hidden_dim in 100 200 300
+    for hidden_dim in 200 300
     do
-      for optimizer in ADAM AdamW SGD
+      for optimizer in Adam AdamW
       do
-      for l_r in 0.01 0.001 0.02
+      for l_r in 0.01 0.001
       do
-       for batch_size in 1 2 8 32 128
+       for batch_size in 1 32 128
        do
        echo "Output:"
         echo "part${part}_task${task}_hiddendim${hidden_dim}_embeddingdim${embedding_dim}_optim_${optimizer}_lr${l_r}_batch_size${batch_size}"
